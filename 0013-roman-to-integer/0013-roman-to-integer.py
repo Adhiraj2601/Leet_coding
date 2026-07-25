3a01776d0713e668d0e21cb4fbbf010fbd726286
Adhiraj <138191602+Adhiraj2601@users.noreply.github.com>
2026-07-25 15:20:20 +0530

    Time: 5 ms (51.56%), Space: 19.1 MB (88.64%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,26 +1,14 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        dict = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-        num=0
-        i = 0
-        while i<len(s):
-            if i<len(s)-1:
-                if s[i]=='I' and (s[i+1]=='V' or s[i+1]=='X'):
-                    num+=dict[s[i+1]]-1
-                    i+=2
-                elif s[i]=='X' and (s[i+1]=='L' or s[i+1]=='C'):
-                    num+=dict[s[i+1]]-10
-                    i+=2
-                elif s[i]=='C' and (s[i+1]=='D' or s[i+1]=='M'):
-                    num+=dict[s[i+1]]-100
-                    i+=2
-                else:
-                    num+=dict[s[i]]
-                    i+=1
+        mapping = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+        num = 0
+        
+        for i in range(len(s)):
+            if i + 1 < len(s) and mapping[s[i]] < mapping[s[i + 1]]:
+                num -= mapping[s[i]]
             else:
-                num+=dict[s[i]]
-                i+=1
-
+                num += mapping[s[i]]
+                
         return num
 
             
