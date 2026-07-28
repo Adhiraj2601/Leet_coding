b65f314e3e2397157961817b2d10685d7adf2241
Adhiraj <138191602+Adhiraj2601@users.noreply.github.com>
2026-07-28 07:42:41 +0530

    Time: 286 ms (46.08%), Space: 20.5 MB (99.08%) - LeetHub

--- 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
+++ 3517-smallest-palindromic-rearrangement-i/3517-smallest-palindromic-rearrangement-i.py
@@ -4,12 +4,8 @@ class Solution:
         if l==1:
             return s
         mid = l//2
-        min_heap = []
         res  = ''
-        for i in range(mid):
-            heapq.heappush(min_heap,s[i])
-        for i in range(mid):
-            res+=heapq.heappop(min_heap)
+        res = "".join(sorted(s[:mid]))
         if l%2==0:
             res+=res[::-1]
         else:
