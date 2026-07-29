05a7db1dcdf4fbd208e724dcb012a516f20e8342
Adhiraj <138191602+Adhiraj2601@users.noreply.github.com>
2026-07-29 16:10:42 +0530

    Time: 0 ms (100%), Space: 17.4 MB (100%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.py
@@ -0,0 +1,11 @@
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        if digits[-1]!=9:
+            digits[-1] = digits[-1]+1
+            return digits 
+        else:
+            number = int("".join(map(str, digits)))
+            number = number+1
+            return list(map(int, str(number)))
+
+        
