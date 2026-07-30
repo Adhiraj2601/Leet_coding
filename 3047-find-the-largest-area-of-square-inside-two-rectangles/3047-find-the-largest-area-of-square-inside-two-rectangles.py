68f165052ff2cdd70e2e391d58128837e49591c7
Adhiraj <adhirajsengar2601@gmail.com>
2026-07-30 11:13:15 +0530

    Time: 2185 ms (86.2%), Space: 19.9 MB (27.3%) - LeetHub

---
+++ 3047-find-the-largest-area-of-square-inside-two-rectangles/3047-find-the-largest-area-of-square-inside-two-rectangles.py
@@ -0,0 +1,23 @@
+class Solution:
+    def largestSquareArea(self, bottomLeft: List[List[int]], topRight: List[List[int]]) -> int:
+        n = len(bottomLeft)
+        max_area = 0
+
+        for i in range(n):
+            x1, y1 = bottomLeft[i]
+            x2, y2 = topRight[i]
+
+            for j in range(i + 1, n):
+                x3, y3 = bottomLeft[j]
+                x4, y4 = topRight[j]
+
+                left = max(x1, x3)
+                right = min(x2, x4)
+                bottom = max(y1, y3)
+                top = min(y2, y4)
+
+                if right > left and top > bottom:
+                    side = min(right - left, top - bottom)
+                    max_area = max(max_area, side * side)
+
+        return max_area
