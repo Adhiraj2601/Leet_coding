f2a58c1c726105de025bdd68c23f5e144d28fd5d
Adhiraj <adhirajsengar2601@gmail.com>
2026-08-02 11:27:24 +0530

    Time: 453 ms (98.95%), Space: 28.5 MB (54.87%) - LeetHub

---
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reversePairs(self, nums: List[int]) -> int:
+        s = SortedList()
+        res = 0
+        for a in nums:
+            res += len(s) - s.bisect_right(a * 2)
+            s.add(a)
+        return res
+ 
+
+
+        
