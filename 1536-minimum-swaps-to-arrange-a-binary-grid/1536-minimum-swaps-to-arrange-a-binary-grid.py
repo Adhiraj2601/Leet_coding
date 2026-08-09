14e764ab29632614f15595a1b1ffa2e0f1e372f1
Adhiraj <adhirajsengar2601@gmail.com>
2026-08-09 17:54:25 +0530

    Time: 11 ms (43.15%), Space: 21 MB (20.72%) - LeetHub

---
+++ 1536-minimum-swaps-to-arrange-a-binary-grid/1536-minimum-swaps-to-arrange-a-binary-grid.py
@@ -0,0 +1,30 @@
+class Solution:
+    def minSwaps(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        trailing_zeros = []
+        for row in grid:
+            count = 0
+            for i in range(n - 1, -1, -1):
+                if row[i] == 0:
+                    count += 1
+                else:
+                    break
+            trailing_zeros.append(count)
+        
+        swaps = 0
+        for i in range(n):
+            target_zeros = n - 1 - i
+            found = False
+            for j in range(i, n):
+                if trailing_zeros[j] >= target_zeros:
+                  
+                    val = trailing_zeros.pop(j)
+                    trailing_zeros.insert(i, val)
+                    swaps += (j - i)
+                    found = True
+                    break
+            
+            if not found:
+                return -1
+                
+        return swaps
