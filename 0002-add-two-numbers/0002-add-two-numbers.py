c38035e0d2d30558e7e1773a5f7b792a91e5bf29
Adhiraj <adhirajsengar2601@gmail.com>
2026-08-09 20:52:16 +0530

    Time: 3 ms (64.98%), Space: 19.4 MB (10.76%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,23 +5,51 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        current1 = l1
+        len1=0
+        while current1 is not None:
+            len1+=1
+            current1 = current1.next
+
+        current2 = l2
+        len2=0
+        while current2 is not None:
+            len2+=1
+            current2 = current2.next
+
+        if len1<len2:
+            max = l2
+            min = l1
+        else:
+            max = l1
+            min = l2
+
         dummy = ListNode(0)
-        curr = dummy
+        curr=dummy
         carry = 0
 
-        while l1 or l2 or carry:
-            val1 = l1.val if l1 else 0
-            val2 = l2.val if l2 else 0
+        while min is not None:
+            current = int((max.val+min.val+carry)%10)
 
-            total = val1 + val2 + carry
-            carry = total // 10
+            curr.next = ListNode(current)
+            curr=curr.next
 
-            curr.next = ListNode(total % 10)
-            curr = curr.next
+            carry = int((max.val+min.val+carry)/10)
 
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
+            max = max.next
+            min = min.next
+        while max is not None:
+            current = int((max.val+carry)%10)
 
+            curr.next = ListNode(current)
+            curr=curr.next
+
+            carry = int((max.val+carry)/10)
+
+            max = max.next
+        if carry>0:
+            curr.next = ListNode(carry)
+      
         return dummy.next
             
 
