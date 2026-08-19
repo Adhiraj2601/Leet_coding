28c1ec6a79ff79d97b0e2fe7e43ff20cd1454479
Adhiraj <adhirajsengar2601@gmail.com>
2026-08-19 14:52:38 +0530

    Time: 200 ms (31.08%), Space: 20 MB (13.95%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -7,9 +7,8 @@ class Solution:
             if s[right] not in dict:
                 dict[s[right]]=right
             else:
-                d = right-left
                 left = max(left, dict[s[right]] + 1)
-                
+
             dict[s[right]]=right
             max_len = max(max_len, right - left + 1)
         return max_len
